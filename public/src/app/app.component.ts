99faa5135436c1fda102a22eba6a96a80c466657
Cameron Lee <cameronlee@Camerons-MacBook-Pro.local>
2019-07-12 14:30:40 -0700

    services included

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'public';
+    title = 'public';
+    constructor(private _httpService: HttpService) { }
+    getAllTask() {
+        let taskObservable = this._httpService.getTasks()
+        taskObservable.subscribe(
+            data => {
+                console.log('Got your tasks:', data);
+                this.tasks = data['tasks']
+            }
+        )
+    }
 }
